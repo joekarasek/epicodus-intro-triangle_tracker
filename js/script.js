d9bac0d8f61152a34614efc41891d27143fce0f7
Joshua Gustafson and Joseph Karasek <silversevenfox@yahoo.com>
2016-01-19 10:03:09 -0800

    add feature scalene for new feature

--- js/script.js
+++ js/script.js
@@ -5,5 +5,7 @@ var triangleTracker = function(side1, side2, side3){
     return "equilateral";
   } else if (side1 === side2 || side2 === side3 || side1 === side3) {
     return "isosceles";
+  } else {
+    return "scalene";
   }
 }
